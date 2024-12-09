3b32342df389188ef75ddc2f742e89845cb8c568
Aakash Badiyani <badiyani.aakash@gmail.com>
2024-12-09 11:56:56 -0500

    Food Ordering App Layout.

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+/* 
+    App
+        -Header
+            -Logo
+            -NavItems
+        -Body
+            -Search
+            -Restaurant Container
+                -Restaurant Cards
+        -Footer
+            -Copyright
+            -Links
+*/
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" alt="Logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+  return(
+    <div className="app">
+        <Header/>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout/>);
