94538f851c59c10713fa8f97b344e642e21e910e
Aakash Badiyani <badiyani.aakash@gmail.com>
2024-12-09 14:50:51 -0500

    Restaurant Card and CSS.

--- App.js
+++ App.js
@@ -33,10 +33,42 @@ const Header = () => {
     );
 }
 
+const RestaurantCard = () => {
+    return (
+      <div className="res-card">
+        <img
+          className="res-logo"
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+          alt="res-logo"
+        />
+        <h3>Meghana Foods</h3>
+        <h4>Biryani, North Indian, Asian</h4>
+        <h4>4.4 stars</h4>
+        <h4>38 mins</h4>
+      </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    );
+}
+
 const AppLayout = () => {
   return(
     <div className="app">
         <Header/>
+        <Body/>
     </div>
   );
 }
