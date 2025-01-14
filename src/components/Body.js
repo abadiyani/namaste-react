73c05f4321d7d818406196ba81fc959d0409786a
Aakash Badiyani <badiyani.aakash@gmail.com>
2025-01-14 11:29:09 -0500

    fetching live data using actual api and useEffect, remove mock data.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,23 @@
 //Component import
 import RestaurantCard from "./RestaurantCard";
-//Data import
-import resList from "../../utils/mockData";
 //package import
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Body = () => {
     //state variable creation using -> useState()
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+    //useEffect for fetching data via api
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
 
     return (
         <div className="body">
