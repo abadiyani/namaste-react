62becbdb89eab551a281af9762de5c849926ebb9
Aakash Badiyani <badiyani.aakash@gmail.com>
2025-02-07 12:31:09 -0500

    Added React Router and Error Handling on Routes.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 //packages
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //components
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Error from "./components/Error";
 
 const AppLayout = () => {
-  return(
+  return (
     <div className="app">
-        <Header/>
-        <Body/>
+      <Header />
+      <Body />
     </div>
   );
-}
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLayout/>);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h2>This is the About us Page</h2>
+    </div>
+  );
+};
+
+export default About;
